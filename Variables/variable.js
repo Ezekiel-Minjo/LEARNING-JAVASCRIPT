e8e29dba04de6a3f2c414a8aad378eefb70c1b0e
minjo <ezekielminjo4@gmail.com>
2025-06-19 16:07:28 +0300

    Variables

---
+++ Variables/variable.js
@@ -0,0 +1,28 @@
+/*
+What is a Variable?
+A Variable is a labeled container that holds a value so you can use it or chnage it
+you can use values directly(literals) or store in variables for reuse
+*/
+// using values directly (literals)
+console.log(5);
+// Storing values in a variable:
+
+var age = 5;
+console.log(age);// uses the stored value
+
+/* 
+Real-Life Example:
+Think of a water bottle with a lable that says "juice"
+- The bottle = variable (var drink)
+- The label = variable name (name)
+- The Juice = value ("Mango")
+*/
+ var drink = "Mango";
+ console.log(drink); // Mango
+ /*
+ you can now carry that bottle (variable) anywhere and use what's inside, or even change the juice to something else like "water" later
+
+✅ Summary:
+A variable lets you store and reuse values instead of repeating them. It’s useful when you need to keep track of data in your code, just like labeling things in real life helps you remember what's inside.
+ 
+ */
