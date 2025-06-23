0606853c001c8a0e0b20c5d3832ababa0172ab30
minjo <ezekielminjo4@gmail.com>
2025-06-23 11:54:09 +0300

    iterator

---
+++ iterator/index1.js
@@ -0,0 +1,18 @@
+const myIterator = {
+    current: 1,
+    last: 3,
+    next() {
+        if (this.current <= this.last) {
+            return {value: this.current++, done: false};
+        } else {
+            return { done: true};
+        }
+    },
+     [Symbol.iterator]() {
+        return this;
+     }
+};
+
+for (let num of myIterator) {
+    console.log(`for...of got: ${num}`);
+}
