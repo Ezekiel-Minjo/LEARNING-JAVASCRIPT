cd9a7c2992cda45f0cc92f718cfc53c6fb904cad
minjo <ezekielminjo4@gmail.com>
2025-06-27 01:02:57 +0300

    ...

---
+++ Fundamental of JS/ternary.js
@@ -0,0 +1,5 @@
+const age = 13;
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+const drink = age >= 18 ? 'wine' : "water";
+console.log(drink);
+console.log(`I like to drink ${age >= 18 ? 'wine' : "water"}`);
