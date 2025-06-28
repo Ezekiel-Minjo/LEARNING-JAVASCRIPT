0c53b7afdb4185641808eac374ce3b9ddab3de0d
minjo <ezekielminjo4@gmail.com>
2025-06-28 18:12:39 +0300

    ...

--- DOM/project/script.js
+++ DOM/project/script.js
@@ -9,6 +9,9 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
@@ -16,5 +19,11 @@ document.querySelector('.check').addEventListener('click', function() {
     if (!guess) {
         const upDate = document.querySelector('.message').textContent = 'No number!';
         console.log(upDate);
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High!';
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too high';
     }
 })
