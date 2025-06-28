f35c4cf0f97f6c00215574e71cbc33d14e7a30d4
minjo <ezekielminjo4@gmail.com>
2025-06-28 18:30:28 +0300

    ...

--- DOM/project/script.js
+++ DOM/project/script.js
@@ -12,6 +12,8 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
@@ -22,8 +24,24 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct number!';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too High!';
+
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You Lost The Game!';
+            document.querySelector('.score').textContent = 0;
+        }
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too high';
+
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You Lost The Game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
-})
+});
