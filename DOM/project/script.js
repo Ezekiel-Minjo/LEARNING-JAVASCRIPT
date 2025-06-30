315f85c84224e128afdc198c275b9421dc5549ae
minjo <ezekielminjo4@gmail.com>
2025-06-30 11:51:17 +0300

    ...

--- DOM/project/script.js
+++ DOM/project/script.js
@@ -34,24 +34,9 @@ document.querySelector('.check').addEventListener('click', function() {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
         }
-        // when guess is too high
-    } else if (guess > secretNumber) {
-
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too High!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You Lost The Game!';
-            document.querySelector('.score').textContent = 0;
-        }
-
-        // when guess is to low
-        
-    } else if (guess < secretNumber) {
-
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too Low!';
+            document.querySelector('.message').textContent = guess > secretNumber ? 'Too High' : 'Too Low!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
@@ -59,6 +44,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+    // when guess is too high
 });
 
 document.querySelector('.again').addEventListener('click', function(){
