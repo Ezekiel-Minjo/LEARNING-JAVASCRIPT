1b833eaf25933fddb291ee6c109f254c795d7a93
minjo <ezekielminjo4@gmail.com>
2025-07-02 01:31:28 +0300

    TDR

---
+++ hoisting/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Minjo";
+let job = "Student";
+const year = 2001;
+
+// functions
+console.log(add(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function add(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
