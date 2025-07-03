728b553e05d0567973e32fb57b45142644090e19
minjo <ezekielminjo4@gmail.com>
2025-07-03 14:57:15 +0300

    ...

--- DOM/Manipulation/script.js
+++ DOM/Manipulation/script.js
@@ -4,3 +4,10 @@ document.getElementById("paragraph").textContent = "Hello, World";
 let element = document.createElement("p");
 element.textContent = "Hello, there!";
 document.body.appendChild(element);
+
+const button = document.getElementById("my-button");
+button.addEventListener("click", doSomething);
+
+function doSomething() {
+  alert("Hello, World!");
+}
