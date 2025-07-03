50357136b67b9bfedf9df565b92e11ec7213d943
minjo <ezekielminjo4@gmail.com>
2025-07-03 14:42:05 +0300

    Dom Manipulation

---
+++ DOM/Manipulation/script.js
@@ -0,0 +1,6 @@
+"use strict";
+document.getElementById("paragraph").textContent = "Hello, World";
+// use javascript to create a new HTML element
+let element = document.createElement("p");
+element.textContent = "Hello, there!";
+document.body.appendChild(element);
