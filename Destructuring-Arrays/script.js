acf00cbd25683f2a88c1b540b7a8d47e7f3b5c7e
minjo <ezekielminjo4@gmail.com>
2025-07-03 20:20:33 +0300

    unpacking values

--- Destructuring-Arrays/script.js
+++ Destructuring-Arrays/script.js
@@ -1,3 +1,4 @@
+'use strict';
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -5,3 +6,18 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 };
+
+const arr = [2, 3, 4];
+const a = [0];
+const b = [1];
+const c = [2];
+
+const [x, y, z] = arr; // distructuring from the right side
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
