d3e31edcf9377fcbee5d8f02c98ce543aae7cc1a
minjo <ezekielminjo4@gmail.com>
2025-07-06 18:05:09 +0300

    nullish assignment

--- Destructuring-Arrays/object.js
+++ Destructuring-Arrays/object.js
@@ -75,15 +75,23 @@ console.log(o, c);
 
 const rest1 = {
   name: 'Capri',
-  numGuests: 20,
+  // numGuests: 20,
+  numGuests: 0,
 };
 
 const rest2 = {
   name: 'La Piazza',
   owner: 'Giovanni Rossi',
 };
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
 
-rest1.numGuests = rest1.numGuests || 10;
-rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
 console.log(rest1);
 console.log(rest2);
