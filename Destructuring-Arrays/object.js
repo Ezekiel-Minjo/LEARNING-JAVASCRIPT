6d87dd06522a107e12dc9f1886ff51375ca7154b
minjo <ezekielminjo4@gmail.com>
2025-07-06 19:01:48 +0300

    sunday coding

--- Destructuring-Arrays/object.js
+++ Destructuring-Arrays/object.js
@@ -93,5 +93,12 @@ const rest2 = {
 // nullish assignment operator (null or undefined)
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest2.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
 console.log(rest1);
 console.log(rest2);
