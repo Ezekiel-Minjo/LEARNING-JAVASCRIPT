4d82931f034c6e6080c6efb33925e99e7dad9b84
minjo <ezekielminjo4@gmail.com>
2025-07-07 13:41:18 +0300

    Looping Objects: Object Keys, Values and Entries

--- Destructuring-Arrays/object.js
+++ Destructuring-Arrays/object.js
@@ -102,3 +102,24 @@ rest2.owner &&= '<ANONYMOUS>';
 
 console.log(rest1);
 console.log(rest2);
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// properties VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
