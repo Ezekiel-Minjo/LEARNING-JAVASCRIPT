46fc5d2af95c8c274fcb6571ffef2d17455570d4
minjo <ezekielminjo4@gmail.com>
2025-07-10 17:54:38 +0300

    ...

---
+++ leetcode/arr.js
@@ -0,0 +1,26 @@
+// // topics: Arrays Hash Table
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// }
+// console.log(twoSum([2, 7, 11, 15], 9));
+
+// Hash Map Approach
+function twoSum(nums, target) {
+  const numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(nums[i], i);
+  }
+  return [];
+}
+console.log(twoSum([2, 7, 11, 15], 9));
