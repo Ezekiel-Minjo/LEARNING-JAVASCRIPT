5124bcd747bf433545d2cde92557a9ec18341119
minjo <ezekielminjo4@gmail.com>
2025-07-21 14:39:01 +0300

    How Passing Arguments Works

---
+++ 10.A Closer Look at Functions/2.passingArguments.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const flight = "LH234";
+const zeki = {
+  name: "Ezekiel Minjo",
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  (flightNum = "Lh999"), (passenger.name = "Mr. " + passenger.name);
+  if (passenger.passport === 24739479284) {
+    console.log("Checked In");
+  } else {
+    console.log("wrong passport!");
+  }
+};
+checkIn(flight, zeki);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(zeki);
+checkIn(flight, zeki);
