b19dc066c6374bc54c3ff38ddf9cde54be892166
minjo <ezekielminjo4@gmail.com>
2025-07-22 23:58:12 +0300

    challenge 1

---
+++ 10.A Closer Look at Functions/8.challenge1.js
@@ -0,0 +1,15 @@
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get Answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+  },
+};
+poll.registerNewAnswer();
