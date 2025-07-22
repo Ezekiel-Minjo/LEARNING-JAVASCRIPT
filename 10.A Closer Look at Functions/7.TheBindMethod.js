07cb79164494a5395b37af53551260a76a145545
minjo <ezekielminjo4@gmail.com>
2025-07-22 23:32:54 +0300

    the bind method

--- 10.A Closer Look at Functions/7.TheBindMethod.js
+++ 10.A Closer Look at Functions/7.TheBindMethod.js
@@ -67,3 +67,6 @@ lufthansa.buyPlane = function () {
 // Partial application
 const addTax = (rate, value) => value + value * rate;
 console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
