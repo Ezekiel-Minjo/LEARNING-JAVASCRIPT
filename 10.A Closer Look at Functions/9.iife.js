2ff0cda6e506c0caf2c32c12b7b4192514c72ef9
minjo <ezekielminjo4@gmail.com>
2025-07-23 14:41:29 +0300

    Immediately invoked functions Expression

---
+++ 10.A Closer Look at Functions/9.iife.js
@@ -0,0 +1,9 @@
+const runOnce = function () {
+  console.log("This will never run again");
+};
+runOnce();
+
+(function () {
+  console.log("This will never run again");
+});
+(() => console.log("This will never run again"))();
