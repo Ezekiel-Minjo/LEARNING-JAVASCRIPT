d11f2a5182b17c816f6a0cfc1e64dff42e97c1aa
minjo <ezekielminjo4@gmail.com>
2025-07-24 01:03:42 +0300

    forEach with Maps and Sets

---
+++ 11.working with Arrays/4.forEach.js
@@ -0,0 +1,18 @@
+// with maps and sets
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+// map
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// set
+const currenciesUnique = new Set(['USD', 'GDP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${_}: ${value}`);
+});
