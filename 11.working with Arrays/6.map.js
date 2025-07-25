a243d98246489337b8b9d64ed611f8cdb3dce0b9
minjo <ezekielminjo4@gmail.com>
2025-07-25 13:35:48 +0300

    computing usernames

--- 11.working with Arrays/6.map.js
+++ 11.working with Arrays/6.map.js
@@ -1,21 +1,13 @@
-//   if (mov > 0) {
-//     return `Movment ${i + 1}: You deposited ${mov}`;
-//   } else {
-//     return `Movment ${i + 1}: You withdrew ${Math.abs(mov)}`;
-//   }
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 const movementsUSD = movements.map(mov => mov * eurToUsd);
-// const movementsUSD = movements.map(function (mov) {
-//   return mov * eurToUsd;
-// });
 console.log(movements);
 console.log(movementsUSD);
 const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
 console.log(movementsUSDfor);
 
-const movementDiscriptions = movements.map((mov, i, arr) => {
+const movementDiscriptions = movements.map((mov, i) => {
   const transactions =
     mov > 0
       ? `Movment ${i + 1}: You deposited ${mov}`
