c51590bc356aec6560771980f2fab078a4d1761b
minjo <ezekielminjo4@gmail.com>
2025-07-25 16:12:33 +0300

    => $ chaining

--- 11.working with Arrays/11.challenge3.js
+++ 11.working with Arrays/11.challenge3.js
@@ -8,12 +8,12 @@ GOOD LUCK 😀
 */
 //   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 
-const calcAverageHumanAge = function (ages) {
-  const humanAge = ages
+const calcAverageHumanAge = ages =>
+  ages
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
     .filter(age => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-  return console.log(humanAge);
-};
+
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
