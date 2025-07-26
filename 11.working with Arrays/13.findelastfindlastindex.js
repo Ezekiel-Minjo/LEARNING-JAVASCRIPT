97e228a9d12cd633b25ac40c5884bbbf41147411
minjo <ezekielminjo4@gmail.com>
2025-07-26 23:44:27 +0300

    The New findLast and findLastIndex method

---
+++ 11.working with Arrays/13.findelastfindlastindex.js
@@ -0,0 +1,14 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const lastwithdrawal = movements.findLast(mov => mov < 0);
+
+console.log(lastwithdrawal);
+
+const latestLargeMovementIndex = movements.findLastIndex(
+  mov => Math.abs(mov) > 1000
+);
+console.log(latestLargeMovementIndex);
+console.log(
+  `Your latest large movement was ${
+    movements.length - latestLargeMovementIndex
+  } movements ago`
+);
