7f9cfa6cc61ab07eb6280bce4c7b508afff6478d
minjo <ezekielminjo4@gmail.com>
2025-07-31 15:52:08 +0300

    constructor function and the new operator

--- 14.OOP with JavaScript/script.js
+++ 14.OOP with JavaScript/script.js
@@ -1,1 +1,13 @@
 'use strict';
+// Implement OOP: Constructor function and the new operator
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const jonas = new Person('Jonas', 1991); // instance of Person
+const matilda = new Person('Matilda', 2017); // instance of Person
+const jack = new Person('Jack', 1975); // instance of Person
+
+console.log(jonas, matilda, jack);
+console.log(jonas instanceof Person);
