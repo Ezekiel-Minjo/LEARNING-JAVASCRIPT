baa46b7976bcbea542902ea2844512a95ed5556c
minjo <ezekielminjo4@gmail.com>
2025-07-31 23:24:28 +0300

    PROTOTYPES

--- 14.OOP with JavaScript/script.js
+++ 14.OOP with JavaScript/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// Implement OOP: Constructor function and the new operator
+// 1. Implement OOP: Constructor function and the new operator
 const Person = function (firstName, birthYear) {
   // instance properties
   this.firstName = firstName;
@@ -8,6 +8,25 @@ const Person = function (firstName, birthYear) {
 const jonas = new Person('Jonas', 1991); // instance of Person
 const matilda = new Person('Matilda', 2017); // instance of Person
 const jack = new Person('Jack', 1975); // instance of Person
-
 console.log(jonas, matilda, jack);
 console.log(jonas instanceof Person);
+
+// 2. prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+// setting properties on the prototype
+Person.prototype.species = 'Home Sapiens';
+console.log(jonas.species, matilda.species, jack.species);
+
+console.log(jonas.hasOwnProperty('firstName')); // checking for objects own property
